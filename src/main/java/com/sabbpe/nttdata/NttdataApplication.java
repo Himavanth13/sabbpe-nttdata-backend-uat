a4ecb8cc972c0d4a5096abb69937f16e7f461a8f
Surya Kenguva <suryakenguva143@gmail.com>
2025-12-05 10:38:10 +0530

    Added configure method in application.java

--- src/main/java/com/sabbpe/nttdata/NttdataApplication.java
+++ src/main/java/com/sabbpe/nttdata/NttdataApplication.java
@@ -2,10 +2,16 @@ package com.sabbpe.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class NttdataApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NttdataApplication.class);
+    }
 
 	public static void main(String[] args){
 		SpringApplication.run(NttdataApplication.class, args);
